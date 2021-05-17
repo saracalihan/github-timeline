5686380a0400ce4e55aab14a56e0aeb325cc172c
saracalihan <saracalihan@gmail.com>
2021-05-17 14:32:15 +0300

    fix linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 12
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12
   },
-  "rules": {
-    "indent": ["error", 2],
-    'quotes': ["error", "single"],
-    'semi': ["error", "always"],
-    'no-multi-spaces': "error",
-    'comma-spacing': ["error", { "before": false, "after": true }],
-    'implicit-arrow-linebreak': ["error", "beside"],
+  'ignorePatterns': ['js/*.min.js'],
+  'rules': {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-multi-spaces': 'error',
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'implicit-arrow-linebreak': ['error', 'beside'],
   }
 };
